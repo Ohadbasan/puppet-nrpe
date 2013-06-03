57b4bd97efedeec2c154be534ed0b386a012fae0
Ohad Basan <obasan@redhat.com>
2013-06-03 15:38:57 +0300

    puppet: add nrpe.cfg template with file puppet type

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,6 +1,11 @@
 class nrpe::install {
   nrpe_packages = ["nrpe", "nagios-plugins-nrpe"]
+  
   package { $nrpe_packages
     ensure => latest,
   }
+
+  file { '/etc/nagios/nrpe.cfg':
+    ensure => present,
+    source => "puppet:///modules/puppet-nrpe/nrpe.cfg",
 }
